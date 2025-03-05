1f1e55e899534ae1a702399f238745286d3eb678
DoanVietQuan <cocailinday105@gmail.com>
2025-03-05 17:44:28 +0700

    Bộ lắng nghe ẩn danh

---
+++ Ex5_AddSubMulDiv_Var/app/src/main/java/com/dvq/ex5_addsubmuldiv_var/MainActivity.java
@@ -0,0 +1,48 @@
+package com.dvq.ex5_addsubmuldiv_var;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    //Khai báo các đối tượng tương ứng với các điều khiển (view) cần thao tác
+    EditText edtSoA;
+    EditText edtSoB;
+    Button btnCong, btnTru, btnNhan, btnChia;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.textViewKQ), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TimView();
+    }
+
+    View.OnClickListener boLangNghe_XuLyCong = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String strSo1 = edtSoA.getText().toString();
+            String
+        }
+    }
+    void TimView(){
+        edtSoA = (EditText) findViewById(R.id.editTextA);
+        edtSoB = (EditText) findViewById(R.id.editTextB);
+        btnCong = (Button) findViewById(R.id.btnCong);
+        btnTru = (Button) findViewById(R.id.btnTru);
+        btnNhan = (Button) findViewById(R.id.btnNhan);
+        btnChia = (Button) findViewById(R.id.btnChia);
+    }
+}

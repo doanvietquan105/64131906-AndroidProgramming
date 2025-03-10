885b0f40b3adeb41237bcc352a89599154bf5a64
DoanVietQuan <cocailinday105@gmail.com>
2025-03-10 21:17:49 +0700

    Update MainActivity.java

--- Ex7_IntentLogin/app/src/main/java/com/dvq/ex7_intentlogin/MainActivity.java
+++ Ex7_IntentLogin/app/src/main/java/com/dvq/ex7_intentlogin/MainActivity.java
@@ -1,6 +1,9 @@
 package com.dvq.ex7_intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button btnDangNhap = (Button)
+        Button btnDangNhap = (Button) findViewById(R.id.btnLogin);
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Lệnh xử lý ở đây
+                Intent iLogin = new Intent(MainActivity.this, LoginActivity.class);
+                androi
+            }
+        });
     }
 }

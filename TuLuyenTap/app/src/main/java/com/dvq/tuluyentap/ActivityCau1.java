39ce119fd8407c5661457b73ba185754b197e107
DoanVietQuan <cocailinday105@gmail.com>
2025-04-04 11:20:37 +0700

    Xong bài 1

---
+++ TuLuyenTap/app/src/main/java/com/dvq/tuluyentap/ActivityCau1.java
@@ -0,0 +1,56 @@
+package com.dvq.tuluyentap;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ActivityCau1 extends AppCompatActivity {
+
+    EditText editTextSoA;
+    EditText editTextSoB;
+    Button nutCong;
+    EditText editTextKQ;
+
+    void TimDieuKhien() {
+        editTextSoA = findViewById(R.id.edtA);
+        editTextSoB = findViewById(R.id.edtB);
+        editTextKQ = findViewById(R.id.edtKQ);
+        nutCong = findViewById(R.id.btnCong);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cau1);
+        TimDieuKhien();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        nutCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XuLyCong();
+            }
+        });
+    }
+
+    void XuLyCong() {
+        String so1 = editTextSoA.getText().toString();
+        String so2 = editTextSoB.getText().toString();
+        float num1 = Float.parseFloat(so1);
+        float num2 = Float.parseFloat(so2);
+        float tong = num1 + num2;
+        String chuoiKQ = String.valueOf(tong);
+        editTextKQ.setText(chuoiKQ);
+    }
+}
---
+++ TuLuyenTap/app/src/main/java/com/dvq/tuluyentap/MainActivity.java
@@ -0,0 +1,43 @@
+package com.dvq.tuluyentap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button cau1, cau2, cau3, cau4;
+    void TimDieuKhien() {
+        cau1 = findViewById(R.id.btnCau1);
+        cau2 = findViewById(R.id.btnCau2);
+        cau3 = findViewById(R.id.btnCau3);
+        cau4 = findViewById(R.id.btnCau4);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        TimDieuKhien();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        cau1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Activity1 = new Intent(MainActivity.this, ActivityCau1.class);
+                startActivity(Activity1);
+            }
+        });
+    }
+}

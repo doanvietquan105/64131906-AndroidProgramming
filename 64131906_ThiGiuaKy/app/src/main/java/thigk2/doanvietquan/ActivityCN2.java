384ccec27e674122a0910134fb17a135958355f8
DoanVietQuan <cocailinday105@gmail.com>
2025-04-04 15:41:53 +0700

    Done about me

--- 64131906_ThiGiuaKy/app/src/main/java/thigk2/doanvietquan/ActivityCN2.java
+++ 64131906_ThiGiuaKy/app/src/main/java/thigk2/doanvietquan/ActivityCN2.java
@@ -1,9 +1,6 @@
 package thigk2.doanvietquan;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,32 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityCN2 extends AppCompatActivity {
 
-    Button cn2, cn3, cn4, about;
-    void TimeDieuKhien(){
-        cn2 = findViewById(R.id.btnCN2);
-        cn3 = findViewById(R.id.btnCN3);
-        cn4 = findViewById(R.id.btnCN4);
-        about = findViewById(R.id.btnAbout);
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        TimeDieuKhien();
+        setContentView(R.layout.activity_cn2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        about.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent About = new Intent(MainActivity.this, AcitivyAboutMe.class);
-                startActivity(About);
-            }
         });
     }
 }
